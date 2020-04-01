7d9b78f1334b478b3e345b8a58e870b9de2634af
Benjamin-deLaverny <benjamin.de-laverny@etu.unice.fr>
2020-04-01 17:12:49 +0200

    Début de déclaration de déchet

---
+++ app/src/main/java/com/example/no_more_trash/DeclarationDechet.java
@@ -0,0 +1,13 @@
+package com.example.no_more_trash;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DeclarationDechet extends AppCompatActivity {
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.declaration_dechet);
+    }
+}
--- app/src/main/java/com/example/no_more_trash/MainActivity.java
+++ app/src/main/java/com/example/no_more_trash/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.no_more_trash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Bouton pour chercher un déchetterie
         Button dechetterie = findViewById(R.id.dechtterie);
+        dechetterie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                declarationDechet(v);
+            }
+        });
         Button decharge = findViewById(R.id.decharge);
+    }
+
+    public void declarationDechet(View view){
+        Intent gameActivity = new Intent(MainActivity.this, DeclarationDechet.class);
+        startActivity(gameActivity);
     }
 }
