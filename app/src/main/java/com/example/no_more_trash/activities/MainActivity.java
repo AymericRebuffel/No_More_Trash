e9a21121f5d3de3a9b11063281d22c0392bed6d6
AymericRebuffel <aymeric.rebuffel@etu.unice.fr>
2020-04-29 14:41:07 +0200

    Ajout d'un test pour l'écriture sur fichier

--- app/src/main/java/com/example/no_more_trash/activities/MainActivity.java
+++ app/src/main/java/com/example/no_more_trash/activities/MainActivity.java
@@ -2,11 +2,22 @@ package com.example.no_more_trash.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.no_more_trash.R;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
+
         findViewById(R.id.admin).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,5 +39,61 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, HomeUser.class));
             }
         });
+
+        //On reset ce qu'il y a écrit dans le context (ici celui de l'application mais j'imagine
+        // que y a moyen d'en choisir d'autre
+        writeToFile("",getApplicationContext());
+
+        //Lorsqu'on appuie sur le bouton, on sauegarde ce qu'il y aait écrit avant
+        // puis on le réécrit en rajoutant plus 1 à la fin.
+        findViewById(R.id.testFileWritting).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = readFromFile(getApplicationContext());
+                writeToFile(data+"+1", getApplicationContext());
+                ((Button) findViewById(R.id.testFileWritting)).setText(readFromFile(getApplicationContext()));
+            }
+        });
+
+    }
+    private void writeToFile(String data, Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("config.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    private String readFromFile(Context context) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput("config.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append("").append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
     }
 }
