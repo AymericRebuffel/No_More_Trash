4810d553363ee0581868677c657397cb1f7ac6fd
Benjamin Coux <benjamin.coux@etu.unice.fr>
2020-05-07 16:23:11 +0100

    ajout pin de la position du telephone sur la carte

--- app/src/main/java/com/example/no_more_trash/activities/Map_Activity.java
+++ app/src/main/java/com/example/no_more_trash/activities/Map_Activity.java
@@ -23,6 +23,7 @@ import androidx.core.app.ActivityCompat;
 
 import com.example.no_more_trash.R;
 
+import org.osmdroid.api.IGeoPoint;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
@@ -44,7 +45,10 @@ public class Map_Activity extends AppCompatActivity {
     private TextView textView;
     private LocationManager locationManager;
     private LocationListener locationListener;
-    private Location mylocation;
+    private double longitude;
+    private  double lattitude;
+   // private Location mylocation;
+    private ArrayList items;
 
     @Override
     public void onCreate(Bundle saveInstantState) {
@@ -52,53 +56,44 @@ public class Map_Activity extends AppCompatActivity {
         getSupportActionBar().hide();
         Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
         setContentView(R.layout.map_decheterie);
+        items = new ArrayList<OverlayItem>();
         textView = findViewById(R.id.textView2);
         map = findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
 
-        IMapController mapController = map.getController();
+        final IMapController mapController = map.getController();
         mapController.setZoom(18.0);
         GeoPoint startPoint = new GeoPoint(43.65020, 7.00517);
         mapController.setCenter(startPoint);
 
 
-        ArrayList<OverlayItem> items = new ArrayList<OverlayItem>();
+
         OverlayItem home = new OverlayItem("Une decheterie", "quelque part", new GeoPoint(43.65020, 7.00517));
         Drawable m = home.getMarker(0);
 
         items.add(home);
         items.add(new OverlayItem("Resto", "chez babar", new GeoPoint(43.64950, 7.00517)));
 
-        Marker mymark = new Marker(map);
+        final Marker mymark = new Marker(map);
         mymark.setPosition(new GeoPoint(43.64950, 7.00517));
         mymark.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
         mymark.setIcon(getResources().getDrawable(R.drawable.trash));
-        map.getOverlays().add(mymark);
-        map.invalidate();
-        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(this, items,
-                new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
-                    @Override
-                    public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
-                        //do something
-                        return true;
-                    }
-
-                    @Override
-                    public boolean onItemLongPress(final int index, final OverlayItem item) {
-                        return false;
-                    }
-                });
-        mOverlay.setFocusItemsOnTap(true);
-        map.getOverlays().add(mOverlay);
-
+       // map.getOverlays().add(mymark);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                mylocation=location;
-                textView.append("\n " + location.getLongitude() + " " + location.getLatitude());
+                longitude=location.getLongitude();
+                lattitude=location.getLatitude();
+                textView.append("\n " + longitude + " " + lattitude);
+                Marker me=new Marker(map);
+                me.setPosition(new GeoPoint(lattitude,longitude));
+                me.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                me.setIcon(getDrawable(R.drawable.gpson));
+                map.getOverlays().add(me);
+                mapController.setCenter(me.getPosition());
             }
 
             @Override
@@ -124,6 +119,27 @@ public class Map_Activity extends AppCompatActivity {
             return;
         }
         locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+        map.invalidate();
+        ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(this, items,
+                new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+                    @Override
+                    public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
+                        //do something
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onItemLongPress(final int index, final OverlayItem item) {
+                        return false;
+                    }
+                });
+        mOverlay.setFocusItemsOnTap(true);
+        map.getOverlays().add(mOverlay);
+        if(findPos(items,"me")!=null){
+            mapController.setCenter(findPos(items,"me"));
+        }
+
+
 
     }
     @Override
@@ -143,6 +159,15 @@ public class Map_Activity extends AppCompatActivity {
         List<OverlayItem> ltmp = new ArrayList<>();
         ltmp.add(tmp);
         map.getOverlays().add((Overlay) ltmp);
+    }
+
+    public IGeoPoint findPos(ArrayList<OverlayItem> list,String tag){
+        for(int i=0;i<list.size();i++){
+            if(list.get(i).getTitle().equals(tag)){
+                return list.get(i).getPoint();
+            }
+        }
+        return null;
     }
 
 }
